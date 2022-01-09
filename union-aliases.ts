473eb44da175e135151d2dd221537ccccbcd9b63
Magomed <mamergov01@bk.ru>
2022-01-09 17:55:26 +0300

    anuion-aliases

---
+++ union-aliases.ts
@@ -0,0 +1,29 @@
+type Combinable = number | string
+
+function combine(
+	input1: Combinable,
+	input2: Combinable,
+	resultConversion: 'as-number' | 'as-text',
+) {
+	let result
+
+	if (
+		(typeof input1 === 'number' && typeof input2 === 'number') ||
+		resultConversion === 'as-number'
+	) {
+		result = +input1 + +input2
+	} else {
+		result = input1.toString() + input2.toString()
+	}
+
+	return result
+}
+
+const combinedAges = combine(20, 18, 'as-number')
+console.log(combinedAges)
+
+const combinedStringAges = combine('20', '18', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine('Maga', 'Ramz', 'as-text')
+console.log(combinedNames)
